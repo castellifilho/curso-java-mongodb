c26e9dd81b07ffd65b2f21aa6c7b284ddae058f0
Castelli <isisdare@gmail.com>
2026-05-16 21:05:32 -0300

    Inserção, deleção e atualização de usuário

--- src/main/java/com/castelli/workshopmongo/services/UserService.java
+++ src/main/java/com/castelli/workshopmongo/services/UserService.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.castelli.workshopmongo.domain.User;
+import com.castelli.workshopmongo.dto.UserDTO;
 import com.castelli.workshopmongo.repository.UserRepository;
 import com.castelli.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +26,28 @@ public class UserService {
 		return obj.orElseThrow(() -> 
 	              new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);		
+	}
+		
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());		
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}		
 }
